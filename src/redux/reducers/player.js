b87594f5f926e874b33f4b119da4c94250494745
Daniel Gomes <danielgomesveiga@hotmail.com>
2022-08-09 17:39:34 -0300

    Requisito 9 - Concluido

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,4 +1,4 @@
-import { ADD_PLAYER } from '../actions';
+import { ADD_PLAYER, ADD_CALC } from '../actions';
 
 const INITIAL_STATE = {
   name: '',
@@ -14,6 +14,11 @@ function player(state = INITIAL_STATE, action) {
       ...state,
       name: action.payload.name,
       gravatarEmail: action.payload.email,
+    };
+  case ADD_CALC:
+    return {
+      ...state,
+      score: action.payload,
     };
   default:
     return state;
