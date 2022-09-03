7e4eb7965fdd47321d437838cc3609d77f0a7506
Lauro Pereira <lauropera01@gmail.com>
2022-09-03 01:47:34 -0300

    Updates

---
+++ precache-manifest.f8fb060ca1c17824eb7a8016eddc3f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef58650fee6487063420759c24b8bbb4",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "f0d88e66fa0d1d896b35",
+    "url": "/trivia/static/css/main.74b906c6.chunk.css"
+  },
+  {
+    "revision": "59436bb7b880832d4907",
+    "url": "/trivia/static/js/2.f99f0332.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/trivia/static/js/2.f99f0332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d88e66fa0d1d896b35",
+    "url": "/trivia/static/js/main.561a1a4a.chunk.js"
+  },
+  {
+    "revision": "13da866554f636ecdfac",
+    "url": "/trivia/static/js/runtime-main.62eca463.js"
+  }
+]);
